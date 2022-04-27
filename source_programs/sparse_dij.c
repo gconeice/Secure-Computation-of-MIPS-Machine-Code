342a42f756a152ebf4aa0aba5961ef99a4701a73
gconeice <yyang811@gatech.edu>
2022-04-27 00:49:36 -0400

    dj fix and sample test

--- source_programs/sparse_dij.c
+++ source_programs/sparse_dij.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-#define MAX 10
-#define MAX_INT 100000
+#define MAX 100
+#define MAX_INT 1000000
 int sfe_main(int *a, int *b, int l1, int l2) {
-   int vis[MAX];
-   int dis[MAX];
-   dis[b[0]] = 0;
    int n = a[0];
    int e = a[1];
    int * node = a + 2;
-   int * edge = a + 2 + n;
-   int * weight = a + 2 + n + e;
+   int * edge = a + 2 + 100;
+   int * weight = a + 2 + 400;
+   int vis[MAX];
+   int dis[MAX];
+   for (int i = 0; i < n; i++) dis[i] = MAX_INT;
+   dis[b[0]] = 0;
+   for (int i = 0; i < n; i++) vis[i] = 0;
    for(int i = 0; i < n; ++i) {
       int bestj = -1, bestdis = MAX_INT;
-      for(int j=0; j<n; ++j) {
-         if( (!vis[j]) & dis[j] < bestdis) {
+      for(int j = 0; j < n; ++j) {
+         if( vis[j] == 0 && dis[j] < bestdis) {
             bestj = j;
             bestdis = dis[j];
          }
       }
       vis[bestj] = 1;
-      for(int j = node[bestj]; j <= node[bestj+1]; ++j) {
+      for(int j = node[bestj]; j < node[bestj+1]; ++j) {
          int newDis = bestdis + weight[j];
          if(newDis < dis[edge[j]])
             dis[edge[j]] = newDis;
@@ -31,10 +33,8 @@ int sfe_main(int *a, int *b, int l1, int l2) {
 /* Driver program to test above function */
 int main()
 {
-   int a[20];
-   int b[20];
-   int num1=20, num2=20;
-   int total = sfe_main(a, b, num1, num2);
+   int *a, *b;
+   int total = sfe_main(a, b, 1000, 10);
    return 0;
 }
 
